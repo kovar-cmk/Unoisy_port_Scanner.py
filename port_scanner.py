f512e6ea9279881920c354111d78adeaf6360667
kovar-cmk <69486825+kovar-cmk@users.noreply.github.com>
2023-05-08 00:14:09 +0100

    Update port_scanner.py
    in this updated version i added an option that allows for scanning all 65000 ports without threads

--- port_scanner.py
+++ port_scanner.py
@@ -7,56 +7,61 @@ date: 4/21/2023
 version 0.1
 initial build
 """
+
+
 import socket
 import argparse
 import threading
-
-def connection_scan(target_ip, target_port):
-    ###Attempt to create a socket connection with a given target ip
-    #### if successful, the port is open. if not the port is closed
+def connection_scan(IP, port):
     try:
-        connect_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect_socket.connect((target_ip, target_port))
-        connect_socket.send(b'Banner_query\r\n')
-        print("[+]{}/TCP open ".format(target_port))
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.connect((IP, port))
+        """" here what we need to do to request for banner or to add an option for banner grabbing to to add 
+                    only one line which is connection_scann.send(b'Banner_query\r\n')
+               """
+        connection.send(b'Banner_query\r\n')
+        results = connection.recv(100)
+        ##and then we are going to capture the result or the banner response let's say in a variable called result
+        print("[*] the port {} is open".format(port))
+        print("[-] {}".format(results))
+        return True
+
     except OSError:
-        print("[-]{}/TCP closed".format(target_port))
+        print("[*\] the port {} is closed".format(port))
+        return False
+
     finally:
-        connect_socket.close() # Ensures the connection is closed
-
-def Port_Scan(t_ip, t_port):
-    """"
-    Scan the indicated ports for status.
-    First, it attempts to resolve the IP address of a provided hostname, then enumerates through the ports.
-    """
+        connection.close()
+def port_scanning(target, port_num):
     try:
-        target_ip  = socket.gethostbyname(t_ip)
-        print("[*] Scan Results for: {}".format(t_ip))
-        connection_scan(t_ip, int(t_port))
+        target_ip = socket.gethostbyname(target)
+        print("[+] Scan Result for {}".format(target),"on port {}: ".format(port_num))
+        connection_scan(target_ip, int(port_num))
+
     except OSError:
-        print("[*] Cannot resove {}: Unkown Host".format(t_ip))
-        return # Exits scan if tatget ip is not resolved
-def scan_arg():
-        """"
-         to Allow us to add some arguments for our scan
+        print("[-] Cannot Resolve the host {}".format(target))
+        return # Exit  Scan if the target is not resolved
 
-        """
-        arg = argparse.ArgumentParser(description="TCp port Scanner. Accepts a hostname/IP addess and list of ports to "
-                                                  "scan. Attempts to identify the service running on a port.")
-        arg.add_argument("-t", "--host", nargs="?", help="Host IP address ")
-        arg.add_argument("-p", "--ports", nargs="?",help="comma separated port list, such as '20,21,22'")
-        var_args = vars(arg.parse_args()) # Covnert argument into a dictionary
-        return var_args
-if __name__ == "__main__" :
+
+def connection_argument():
+    arguments = argparse.ArgumentParser(description="these are the options we cann use alongside with this program")
+    arguments.add_argument("-t", "--host", nargs="?", help="triggers the host value")
+    arguments.add_argument("-p", "--ports", nargs="?", help="triggers the port value")
+    arguments.add_argument("-a", "--all", action="store_true" , help="an option to scann all the 65000 ports ")
+    argument_vars = vars(arguments.parse_args()) # Convert argument namespace to dictionary
+
+    return argument_vars
+
+
+if __name__ == '__main__':
     try:
-        user_args = scan_arg()
-        host = user_args["host"]
-        port_list = user_args["ports"].split(",") # Make a list from port numbers
-        for port in port_list:
-            Port_Scan(host, port)
+       user_args = connection_argument()
+       host = user_args["host"]
+       if user_args["all"]:
+           port_list = range(1, 65000)
+       else:
+           port_list = user_args["ports"].split(",")# Make a list from port numbers
+       for port in port_list:
+           port_scanning(host, int(port))
     except AttributeError:
         print("Error. Please provide the command-line arguments before running.")
-
-
-
-
